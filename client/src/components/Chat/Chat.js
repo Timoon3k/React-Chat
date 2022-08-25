234f1407dff78bcf2bc459f983b08223dde37442
Tomasz <tomasz.majewski1994@gmail.com>
2022-08-25 22:18:39 +0200

    make it better

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
 import queryString from 'query-string';
 import { Navigate, useLocation } from 'react-router-dom';
-
 import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
-
 import './Chat.css';
 import { SocketContext } from '../../context/socket';
 
@@ -28,13 +26,21 @@ const Chat = () => {
 	}, [location.search, socket]);
 
 	useEffect(() => {
-		socket.on('message', (message) => {
+		const addMessage = (message) => {
 			setMessages((messages) => [...messages, message]);
-		});
+		};
 
-		socket.on('roomData', ({ users }) => {
+		const setUsersInRoom = ({ users }) => {
 			setUsers(users);
-		});
+		};
+
+		socket.on('message', addMessage);
+		socket.on('roomData', setUsersInRoom);
+
+		return () => {
+			socket.off('message', addMessage);
+			socket.off('roomData', setUsersInRoom);
+		};
 	}, [socket]);
 
 	const sendMessage = (event) => {
