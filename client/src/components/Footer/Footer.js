82626597c3e03e5529e61755b20f062cc33468f6
Tomasz <tomasz.majewski1994@gmail.com>
2022-09-10 14:04:38 +0200

    fix footerYear

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './Footer.css';
-import './Footer__Year';
 
 const Footer = () => {
 	return (
 		<div>
-			<h4 className='footer'>
-				<span className='footer__year'></span> © Design by Tomasz Majewski
-			</h4>
+			<h4 className='footer'>{new Date().getFullYear()} © Design by Tomasz Majewski</h4>
 		</div>
 	);
 };
--- client/src/components/Footer/Footer__Year.js
+++
@@ -1,8 +0,0 @@
-const footerYear = document.querySelector('.footer__year');
-
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.innerText = year;
-};
-
-handleCurrentYear();
