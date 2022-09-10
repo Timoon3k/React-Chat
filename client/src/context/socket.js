8ba3381ee092a0020da847caae05f6dbf5e31826
Tomasz <tomasz.majewski1994@gmail.com>
2022-09-10 13:39:13 +0200

    add description "SEO"

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -2,5 +2,5 @@ import socketio from 'socket.io-client';
 import { createContext } from 'react';
 const ENDPOINT = 'https://tomaszmajewski-chat.herokuapp.com';
 
-export const socket = socketio.connect(ENDPOINT, { transports: ['websocket', 'pooling'] });
+export const socket = socketio.connect(ENDPOINT, { transports: ['websocket'] });
 export const SocketContext = createContext();
