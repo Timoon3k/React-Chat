0fa51c0974b44cf4d1b290fa56365ee7b920779e
Tomasz <tomasz.majewski1994@gmail.com>
2022-09-10 13:17:15 +0200

    fix procfile

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const router = Router();
 
 router.get('/', (req, res) => {
 	res.redirect('https://tomaszmajewski-chat.netlify.app');
+	res.send('Serwer został uruchomiony');
 });
 
 export default router;
